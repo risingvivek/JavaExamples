44965833a350687337765ff83c8f99b9c4f75914
Vivek Kumar <vivekkumar@Viveks-MacBook-Pro.local>
2020-01-03 05:19:36 +0000

    Adding application

---
+++ src/main/java/com/vk/quasys/Application.java
@@ -0,0 +1,17 @@
+package com.vk.quasys;
+
+import java.math.BigDecimal;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+    
+}
